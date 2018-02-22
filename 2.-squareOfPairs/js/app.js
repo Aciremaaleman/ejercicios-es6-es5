530ace5d3effc9a2621ebb20bdbe65d047c89c44
aciremaaleman <aciremaaleman@outlook.es>
2018-02-21 22:22:16 -0600

    Subiendo ejercicios ecma6

--- 2.-squareOfPairs/js/app.js
+++ 2.-squareOfPairs/js/app.js
@@ -4,18 +4,23 @@
 
 // declara las variables ES5
 
-var array = [2, 4, 6];
+// var array = [2, 4, 6];
 // var array = [-3, 2, -8, 12, 5];
 // var array = [1, 2, 3, 4, 5];
 
 
 // declaración de la funcion 
-
-function square () {
-  // escribe tu código aqui...
-}
-
-square(array);
+// Nota: los signos menos en consola desaparecen y el -3 como no es par tambien aunque el 5 que si lo es no
+// function square () {
+//   for (var i = 0; i < array.length; i++){
+//     if (array[i] >= 0 && array[i] <= 0 || array[i] % 2 == 0){
+//       console.log(array[i] * array[i]);
+//     } else if (array[i] % 2 == 1) {
+//       console.log(array[i]);
+//   }
+//  }
+// }
+// square(array);
 
 
                       //  ES6
@@ -25,16 +30,24 @@ square(array);
 
 // declara variables (let-const)
 
+// const array = [2, 4, 6];
+const array = [-3, 2, -8, 12, 5];
+// const array = [1, 2, 3, 4, 5];
 
 // declara la funcion y ejecuta el código
+const square = array => {
+  let num = array.map(array => {
+    if (array >= 0 && array <= 0 || array % 2 == 0) {
+      return array * array
+    } else {
+      return array
+    }
+  });
+  console.log(num);
+};  
+square(array);
 
-
-
-
-
-
-
-
-
-// ejecuta la funcion 
+// me regresa el array elevado al cuadrado falta un if
+// const num = array.map(item => Math.pow(item,2));
+// console.log(num)
 
--- 5.-Login/js/app.js
+++ 5.-Login/js/app.js
@@ -1,19 +1,35 @@
 // En este ejercicio deberas crear tu solción en ES5, asegurarte que funcione 
 
 // declara el prompt
+// var askPassword = prompt('¿Cuál es tu contraseña?');
+// var password = 'laboratoria';
  
-function user () {
-// escribe tu código aqui...
-}
+// function user () {
+//     if(askPassword === password){
+//         console.log('true');
+//     } else if (askPassword !== password){
+//         console.log('false');
+//     } else if (askPassword === ""){
+//         console.log('false');
+//     }
+// // escribe tu código aqui...
 
-
-
-
-
+// }
+// user ();
 
 // comenta todo lo anterior y escribelo en ES6
 
+const askPassword = prompt('¿Cuál es tu contraseña?');
+let password = 'laboratoria';
 
-// user () => {
 
-// }
+const user = password => {
+     if(askPassword === password){
+        console.log('true');
+    } else if (askPassword !== password){
+        console.log('false');
+    } else if (askPassword === ""){
+        console.log('false');
+    }
+}
+user(password);
